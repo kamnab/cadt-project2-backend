ade7a4821e6dfb5efe83379c59bf5915d85d8c2b
kamnab <k.kamnab@gmail.com>
2025-05-31 22:42:12 +0700

    migrate mongodb from atlas to docker in vm

--- db/db.js
+++ db/db.js
@@ -3,12 +3,12 @@
 
 const mongoose = require('mongoose')
 const dbName = "cadt-project2-backend-db"
-const uri = `mongodb+srv://kamnab:P%40ssw0rd%40mdb@cluster0.iz4c7.mongodb.net/${dbName}`
-//`mongodb://localhost:27017/${dbName}`
+// const uri = `mongodb+srv://kamnab:P%40ssw0rd%40mdb@cluster0.iz4c7.mongodb.net/${dbName}`
+const uri = `mongodb://${process.env.DB_HOST}:27017/${dbName}`
 
 async function dbConnect() {
     mongoose.connection.on('connected', () => {
-        console.log("Mongodb Connected")
+        console.log("Mongodb Connected: " + process.env.DB_HOST)
     })
     await mongoose.connect(uri, {
         dbName: dbName
